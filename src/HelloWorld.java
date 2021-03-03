57e9550f483c344ffbe1282b7d478e745fd5d1bb
Nykyta <nikita.germai.520@gmail.com>
2021-03-04 00:05:52 +0200

    Fixed HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main (String [] args) {
-        System.out.println ("Hello World In Git");
+        System.out.println ("Hello World In Error");
     }
 }
